d210961ff2afa593d8322053de2e354ac9bbc32f
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-01 18:33:51 +0100

    Implement Jsx in the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 
 function App() {
-  const title = 'react + vite';
+  const welcome = {
+    greeting: 'Hey',
+    title: 'React',
+  };
+
   return (
     <div>
-      <h1>hello {title} application </h1>
+      <h1>
+        hello {welcome.greeting} {welcome.title} application{' '}
+      </h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
-
 export default App;
