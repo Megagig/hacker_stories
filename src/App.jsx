6fa5dec43508b4ff7784df341801c29a392cb6b7
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-06 15:03:40 +0100

    Adding Lists In react Using js map() method

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Anthony Obi',
+    num_comments: 1,
+    points: 2,
+    objectID: 1,
+  },
+];
+
 function App() {
   const welcome = {
     greeting: 'Hey',
@@ -11,6 +30,21 @@ function App() {
       </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
