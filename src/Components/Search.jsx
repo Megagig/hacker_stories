882cdd3a4107ced29aa0f2f2737c753bec279726
megagig <megagigsoftwaresolutions@gmail.comm>
2024-02-07 21:07:50 +0100

    Add the handler function for events

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,15 @@
 const Search = () => {
+  // handler function in jsx
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };
